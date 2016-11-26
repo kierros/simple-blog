8b2129813d4c6514c97fb2d524e68451a1c08ef2
W <hellomyw@gmail.com>
2016-11-27 00:57:48 +0300

    Update

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
