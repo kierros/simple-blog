e781f2ba8f1ec4c43f797d97af58bfc7aa8aaa5d
W <hellomyw@gmail.com>
2016-11-27 00:35:00 +0300

    Update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
+import django.contrib.auth.views
+
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'', include('blog.urls')),
 ]
