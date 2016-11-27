60c258f251ba7d4c2dcfafe532cf1ebde92e5be9
W <hellomyw@gmail.com>
2016-11-27 20:25:11 +0300

    Closed ability to upload pictures

--- blog/forms.py
+++ blog/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
-    image = forms.ImageField(required=False)
+    #image = forms.ImageField(required=False)
     class Meta:
         model = Post
-        fields = ('title', 'text', 'image')
+        fields = ('title', 'text')
 
 class CommentForm(forms.ModelForm):
 
